c545327ccacbe2b83af845afce1b9a44cd0216e7
drealms <38113110+drealms@users.noreply.github.com>
2022-02-13 20:08:43 +0300

    Add files via upload

--- toen.py
+++ toen.py
@@ -1,6 +1,6 @@
 import subprocess
 while True:
-    inp = input()
+    inp = input('to english \n')
     with open('/home/ubuntu/ruwords', 'a') as file_object:
         file_object.write(inp + '\n')
     process = subprocess.Popen(['trans','-verbose','-s','ru','-t','en'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
